63859b9e7f5910c9a5b7fd3683600f68cf2ffa0e
David Gaspar <davsangas@hotmail.com>
2023-05-05 09:38:22 -0600

    Logic

---
+++ src/Constants.js
@@ -0,0 +1,13 @@
+export const SUPPORTED_LANGUAGES = {
+    en: "English", //en it's a key
+    es: "Spanish",
+    fr: "French",
+    de: "German",
+    it: "Italian",
+    pt: "Portuguese",
+    ru: "Russian",
+    zh: "Chinese",
+    ja: "Japanese",
+    ar: "Arabic",
+}
+
---
+++ src/hooks/useStore.js
@@ -0,0 +1,95 @@
+import { useReducer } from "react";
+import { SUPPORTED_LANGUAGES } from "../Constants";
+//1. initialState
+const initialState = {
+    fromLanguage: "auto",
+    toLanguage: "es",
+    fromText: "",
+    loading: false,
+    result: "",
+};
+//2. Create a reducer
+function reducer(state, action) {
+    const { type, payload } = action;
+
+    if (type === "INTERCHANGE_LANGUAGE") {
+        return {
+            ...state,
+            fromLanguage: state.toLanguage,
+            toLanguage: state.fromLanguage,
+        };
+    }
+
+    if (type === "SET_FROM_LANGUAGE") {
+        return {
+            ...state,
+            fromLanguage: payload,
+        };
+    }
+
+    if (type === "SET_TO_LANGUAGE") {
+        return {
+            ...state,
+            toLanguage: payload,
+        };
+    }
+
+    if (type === "SET_FROM_TEXT") {
+        return {
+            ...state,
+            loading: true,
+            fromText: payload,
+            result: "",
+        };
+    }
+
+    if (type === "SET_RESULT") {
+        return {
+            ...state,
+            loading: false,
+            result: payload,
+        };
+    }
+
+    return state;
+}
+
+export function useStore() {
+    const [{ fromLanguage, toLanguage, fromText, loading, result }, dispatch] =
+        useReducer(reducer, initialState);
+
+    const interChangeLanguages = () => {
+        dispatch({ type: "INTERCHANGE_LANGUAGE" });
+    };
+
+    const setFromLanguage = (payload) => {
+        if(Object.keys(SUPPORTED_LANGUAGES).includes(payload)) {
+            dispatch({ type: "SET_FROM_LANGUAGE", payload });
+        }else {
+            console.error(`Language code ${payload} is not supported`);
+        }
+    };
+
+    const setToLanguage = (payload) => {
+        dispatch({ type: "SET_TO_LANGUAGE", payload });
+    };
+    const setFromText = (payload) => {
+        dispatch({ type: "SET_FROM_TEXT", payload });
+    };
+    const setResult = (payload) => {
+        dispatch({ type: "SET_RESULT", payload });
+    };
+
+    return {
+        fromLanguage,
+        toLanguage,
+        fromText,
+        loading,
+        result,
+        interChangeLanguages,
+        setFromLanguage,
+        setToLanguage,
+        setFromText,
+        setResult,
+    };
+}
