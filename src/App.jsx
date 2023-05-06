70f3ced85df42c539c9e5c0a7d3fddc566cbe555
David Gaspar <davsangas@hotmail.com>
2023-05-06 08:49:18 -0600

    Conditional for auto-lenguage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { FaExchangeAlt } from "react-icons/fa";
 import { useStore } from "./hooks/useStore";
 
-
-
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
-  const { fromLanguage, setFromLanguage } = useStore();
+  const { fromLanguage, toLanguage, interChangeLanguages } = useStore();
   return (
-    <div className="App">
-      <h1 className="text-red-500 font-inter">Hello World</h1>
-      <button
-        onClick={() => {
-          setFromLanguage("es");
-        }}
-      >
-        <FaExchangeAlt />
-      </button>
-      <p>{fromLanguage}</p>
-    </div>
+    <Container fluid>
+      <h1 className="mb-2 font-bold text-2xl font-roboto">Google Clon translate</h1>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button disabled={fromLanguage === 'auto'} variant="link" onClick={interChangeLanguages} className="text-black">
+            <FaExchangeAlt />
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
