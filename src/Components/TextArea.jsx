70839c32ef3d9cd287b2f992d4807824eaf3b972
David Gaspar <davsangas@hotmail.com>
2023-05-13 21:44:33 -0600

    Value

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -2,14 +2,19 @@ import { Form } from "react-bootstrap";
 
 
 export const TextArea = ({ autofocus ,placeholder, loading,  value, onChange }) => {
+    const handleChange = (event) => {
+        onChange(event.target.value);
+    }
     return (
         <Form.Control
             className="bg-transparent border-2 text-gray-200 border-gray-400"
             autoFocus={autofocus}
-            as="textarea"
+            as="textarea" //Element to render
             rows={3}
             placeholder={placeholder}
             style={{ height: '150px', marginTop: '8px' }}
+            value={value}
+            onChange={handleChange}
         />
     )
 }
