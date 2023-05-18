b4d7024620075ca7328f3afb434fe4ac8fbd0ab3
David Gaspar <davsangas@hotmail.com>
2023-05-18 16:55:04 -0600

    useDebounce and funcionalities

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import { SelectorLenguages } from "./Components/SelectorLenguages";
 import { TextArea } from "./Components/TextArea";
 import { useEffect } from "react";
 import { translate } from "./Services/Translate";
+import { useDebounce } from "./Components/useDebounce";
+import { CopyIcon, Microphone } from "./assets/Icons";
+import { Toaster } from "sonner";
+import { toast } from "sonner";
 
 function App() {
   const {
@@ -26,64 +30,101 @@ function App() {
     setToLanguage,
   } = useStore();
 
-  useEffect(() => {
-    if (fromText === '') return
+  const debouncedFromText = useDebounce(fromText, 500);
 
-    translate({fromLanguage, toLanguage, text:fromText})
-    .then(result => {
-      if (result === null) return
-      setResult(result)
-    })
-    .catch(() => setResult('Error'))
-  }, [fromText, fromLanguage, toLanguage])
+  useEffect(() => {
+    if (debouncedFromText === "") return;
+
+    translate({ fromLanguage, toLanguage, text: fromText })
+      .then((result) => {
+        if (result === null) return;
+        setResult(result);
+      })
+      .catch(() => setResult("Error"));
+  }, [debouncedFromText, fromLanguage, toLanguage]);
+
+  const handleCopiedToClipboard = () => {
+    navigator.clipboard.writeText(result);
+    toast.success("Copied to clipboard");
+  };
+
+  const handleVoiced = () => {
+    const utterance = new SpeechSynthesisUtterance(result);
+    utterance.lang = toLanguage;
+    speechSynthesis.speak(utterance);
+  };
 
   return (
-    <Container fluid className="border-3 border-gray-400 p-4 rounded-4 backdrop-blur-sm">
-      <h1 className="mb-2 font-bold text-2xl font-roboto">
-        Google Clon translate
-      </h1>
-      <Row>
-        <Col>
-          <SelectorLenguages
-            onChange={setFromLanguage}
-            value={fromLanguage}
-            showDetect={true}
-          />
-          <TextArea
-            as="textarea"
-            placeholder="Enter text"
-            autoFocus
-            value={fromText}
-            onChange={setFromText}
-          />
-        </Col>
+    <>
+      <Container
+        fluid
+        className="border-3 border-gray-400 p-4 rounded-4 backdrop-blur-sm relative"
+      >
+        <h1 className="mb-2 font-bold text-2xl font-roboto">
+          Google Clon translate
+        </h1>
+        <Row>
+          <Col>
+            <SelectorLenguages
+              onChange={setFromLanguage}
+              value={fromLanguage}
+              showDetect={true}
+            />
+            <TextArea
+              as="textarea"
+              placeholder="Enter text"
+              autoFocus
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Col>
 
-        <Col>
-          <Button
-            variant="link"
-            onClick={interChangeLanguages}
-            className="text-black"
-          >
-            <FaExchangeAlt />
-          </Button>
-        </Col>
+          <Col>
+            <Button
+              variant="link"
+              onClick={interChangeLanguages}
+              className="text-black"
+            >
+              <FaExchangeAlt />
+            </Button>
+          </Col>
 
-        <Col>
-          <SelectorLenguages
-            onChange={setToLanguage}
-            value={toLanguage}
-            showDetect={false}
-          />
-          <TextArea
-            placeholder="Traduction"
-            autoFocus
-            value={result}
-            onChange={setResult}
-            loading={loading}
-          />
-        </Col>
-      </Row>
-    </Container>
+          <Col>
+            <SelectorLenguages
+              onChange={setToLanguage}
+              value={toLanguage}
+              showDetect={false}
+            />
+            <div style={{ position: "relative" }}>
+              <TextArea
+                placeholder="Traduction"
+                autoFocus
+                value={result}
+                onChange={setResult}
+                loading={loading}
+              />
+              <Button
+                id="copy-button"
+                variant="link"
+                style={{ position: "absolute", right: "100px", top: "110px" }}
+                onClick={handleCopiedToClipboard}
+              >
+                {result === "" ? "" : <CopyIcon />}
+              </Button>
+              <Button
+                id="voice-button"
+                variant="link"
+                style={{ position: "absolute", right: "70px", top: "110px" }}
+                onClick={handleVoiced}
+              >
+                {result === "" ? "" : <Microphone />}
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Toaster position="botton-center" expand={true} richColors success />
+    </>
   );
 }
 
--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -1,20 +1,15 @@
 import { Form } from "react-bootstrap";
 
 const getPlaceholder = (loading, result) => {
-    if (loading) return "Translating..."
-    if (result === "")  return "Translation"
-    return "Translation"
-}
+    if (loading) return "Translating...";
+    if (result === "") return "Translation";
+    return "Translation";
+};
 
-export const TextArea = ({
-    autofocus,
-    loading,
-    value,
-    onChange,
-}) => {
+export const TextArea = ({ autofocus, loading, value, onChange }) => {
     const handleChange = (e) => {
         onChange(e.target.value);
-    }
+    };
     return (
         <Form.Control
             className="bg-transparent border-2 text-gray-200 border-gray-400"
